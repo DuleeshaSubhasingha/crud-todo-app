e99f94dd3a71a0aa855aa0a3a42458d64c06a9e6
DuleeshaSubhasingha <duleeshadulmini1999@gmail.com>
2025-07-26 18:36:53 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require 
+
+const app = express();
+app.use(cors({origin:process.env.FRONTEND_URL}))
+app.use(express.json());
+
+const router = require("./routes");
+app.use("/api", router);
+
+const connectDB = require("./connectDb");
+
+const port = 5000;
+
+const startServer = async () => {
+    await connectDB();
+    app.listen(port,()=>{
+        console.log(`server is listening on http://localhost:${port}`);
+    });
+};
+
+
+startServer();
